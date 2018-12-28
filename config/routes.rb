2ab3b982ff55c08511643fa165a29d8f1ef876da
Nestor R Benavidez <nestor.benavidez@gmail.com>
2018-12-28 05:57:32 -0300

    Sessions and users edited

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get 'user_sessions/destroy'
   resources :users
   get 'dashboard/index'
-  root 'dashboard#index'
+  root :to => 'users#index'
+  resources :user_sessions
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
